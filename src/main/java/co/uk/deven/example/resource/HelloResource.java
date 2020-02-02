1e210ea11483e29061fa04b4b79932327ea653be
deven <devjob.2012@gmail.com>
2020-02-02 21:19:50 +0000

    Added

--- src/main/java/co/uk/deven/example/resource/HelloResource.java
+++ src/main/java/co/uk/deven/example/resource/HelloResource.java
@@ -3,10 +3,15 @@ package co.uk.deven.example.resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 public class HelloResource {
-	@GetMapping("/")
-	public String greeting() {
-		return "greeting";
-	}
+    @GetMapping("/")
+    public String greeting() {
+        Optional<Integer> canBeEmpty1 = Optional.of(5);
+        System.out.println(canBeEmpty1.get());
+
+        return "greeting";
+    }
 }
